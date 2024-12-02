2a83a8a838478f68db760da80f7ba85abbe7ff51
enricomaneschi <enrico.maneschi@outlook.it>
2024-12-02 13:41:46 +0100

    NotFoundFix

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, Col, Row } from 'react-bootstrap'
+import { Col, Row } from 'react-bootstrap'
 
 const NotFound = () => (
   <Row className="justify-content-center my-4">
